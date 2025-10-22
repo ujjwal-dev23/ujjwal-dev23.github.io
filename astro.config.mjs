c1a6b1b3d0d42b2c4345e84a83e65b1d212f496d
Ujjwal <ujjwalkandera23@tutamail.com>
2025-10-22 18:03:57 +0530

    complete initial project setup

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,15 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import tailwindcss from '@tailwindcss/vite';
+
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  // site: 'https://<host_url>'
+  trailingSlash: 'ignore',
+
+  output: 'static',
+  vite: {
+    plugins: [tailwindcss()]
+  }
+});
